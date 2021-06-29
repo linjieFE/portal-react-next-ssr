3df721f5d80d8989d70fb3923d2deae3692d4321
linjie.zheng <linjie.zheng@flexiv.com>
2021-06-29 11:34:18 +0800

    [#] config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
+const path = require('path')
 module.exports = {
     i18n: {
       locales: ['en', 'cn'],
       defaultLocale: 'en'
-    }
+    },
+    trailingSlash: true,
+    sassOptions: {
+      includePaths: [path.join(__dirname, 'styles')],
+    },
   }
